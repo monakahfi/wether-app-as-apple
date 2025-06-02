cad5b1a7e269e34fe15ba174fcdc1a1c166cb32b
Mona Kahfi <mandanakahfi@gmail.com>
2025-06-02 21:48:23 +0330

    create base of app

---
+++ wether-apple/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className="w-auto h-20  bg-rose-400 flex justify-center items-center p-2 m-10 md:w-full md:h-auto font-bold ">
+  create weatherApp as apple
+</h1>
+
+    </>
+  )
+}
+
+export default App
